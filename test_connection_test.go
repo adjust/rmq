41f055e1f1cbfa1297ec0369e1ae4800fc4e005d
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2020-05-04 00:56:11 +0200

    Clean up some tests

--- test_connection_test.go
+++ test_connection_test.go
@@ -23,14 +23,12 @@ func (suite *ConnectionSuite) TestConnection(c *C) {
 	c.Check(connection.GetDelivery("things", 0), Equals, "rmq.TestConnection: delivery not found: things[0]")
 	c.Check(connection.GetDelivery("things", 1), Equals, "rmq.TestConnection: delivery not found: things[1]")
 
-	err = queue.Publish("bar")
-	c.Check(err, IsNil)
+	c.Check(queue.Publish("bar"), IsNil)
 	c.Check(connection.GetDelivery("things", 0), Equals, "bar")
 	c.Check(connection.GetDelivery("things", 1), Equals, "rmq.TestConnection: delivery not found: things[1]")
 	c.Check(connection.GetDelivery("things", 2), Equals, "rmq.TestConnection: delivery not found: things[2]")
 
-	err = queue.Publish("foo")
-	c.Check(err, IsNil)
+	c.Check(queue.Publish("foo"), IsNil)
 	c.Check(connection.GetDelivery("things", 0), Equals, "bar")
 	c.Check(connection.GetDelivery("things", 1), Equals, "foo")
 	c.Check(connection.GetDelivery("things", 2), Equals, "rmq.TestConnection: delivery not found: things[2]")
@@ -38,8 +36,7 @@ func (suite *ConnectionSuite) TestConnection(c *C) {
 	connection.Reset()
 	c.Check(connection.GetDelivery("things", 0), Equals, "rmq.TestConnection: delivery not found: things[0]")
 
-	err = queue.Publish("blab")
-	c.Check(err, IsNil)
+	c.Check(queue.Publish("blab"), IsNil)
 	c.Check(connection.GetDelivery("things", 0), Equals, "blab")
 	c.Check(connection.GetDelivery("things", 1), Equals, "rmq.TestConnection: delivery not found: things[1]")
 }
--- test_delivery_test.go
+++ test_delivery_test.go
@@ -16,35 +16,28 @@ type DeliverySuite struct {
 func (suite *DeliverySuite) TestDeliveryPayload(c *C) {
 	var delivery Delivery
 	delivery = NewTestDelivery("p23")
-	err := delivery.Ack()
-	c.Check(err, IsNil)
+	c.Check(delivery.Ack(), IsNil)
 	c.Check(delivery.Payload(), Equals, "p23")
 }
 
 func (suite *DeliverySuite) TestDeliveryAck(c *C) {
 	delivery := NewTestDelivery("p")
 	c.Check(delivery.State, Equals, Unacked)
-	err := delivery.Ack()
-	c.Check(err, IsNil)
+	c.Check(delivery.Ack(), IsNil)
 	c.Check(delivery.State, Equals, Acked)
 
-	err = delivery.Ack()
-	c.Check(err, Equals, ErrorNotFound)
-	err = delivery.Reject()
-	c.Check(err, Equals, ErrorNotFound)
+	c.Check(delivery.Ack(), Equals, ErrorNotFound)
+	c.Check(delivery.Reject(), Equals, ErrorNotFound)
 	c.Check(delivery.State, Equals, Acked)
 }
 
 func (suite *DeliverySuite) TestDeliveryReject(c *C) {
 	delivery := NewTestDelivery("p")
 	c.Check(delivery.State, Equals, Unacked)
-	err := delivery.Reject()
-	c.Check(err, IsNil)
+	c.Check(delivery.Reject(), IsNil)
 	c.Check(delivery.State, Equals, Rejected)
 
-	err = delivery.Reject()
-	c.Check(err, Equals, ErrorNotFound)
-	err = delivery.Ack()
-	c.Check(err, Equals, ErrorNotFound)
+	c.Check(delivery.Reject(), Equals, ErrorNotFound)
+	c.Check(delivery.Ack(), Equals, ErrorNotFound)
 	c.Check(delivery.State, Equals, Rejected)
 }
